f72c7d9552968f32b2021eedb8de610376a469c5
jonfoong <jonahfoong@gmail.com>
2023-03-21 17:29:17 +0100

    update

--- R/label_wrap_gen_both.R
+++ R/label_wrap_gen_both.R
@@ -6,14 +6,20 @@
 #' @param width Maximum number of characters before wrapping the strip.
 #' @param multi_line Whether to display the labels of multiple factors on separate lines.
 #' @param sep String separating variables and values.
-#'
-#' @importFrom ggplot2 label_variable
 
 
 label_wrap_gen_both <- function(width = 10, multi_line = TRUE, sep = ": ") {
   fun <- function(labels) {
     value <- label_value(labels, multi_line = multi_line)
-    variable <- ggplot2:::label_variable(labels, multi_line = multi_line)
+
+    if (multi_line) {
+      row <- as.list(names(labels))
+    }
+    else {
+      row <- list(paste(names(labels), collapse = ", "))
+    }
+    variable <- lapply(row, rep)
+
     if (multi_line) {
       out <- vector("list", length(value))
       for (i in seq_along(out)) {
