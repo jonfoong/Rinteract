a0ca451c16f0cfb1ab5a850f24407ed2eb324d61
jonfoong <jonahfoong@gmail.com>
2023-04-18 16:52:07 +0200

    toydata

--- R/toydata.R
+++ R/toydata.R
@@ -7,9 +7,13 @@
 #' @usage data(toydata)
 #'
 #' @format An object of class \code{"data.frame"}
-
+#'
+#' #' \describe{
+#'   \item{Col1}{description of Col1}
+#'   \item{Col2}{description of Col2}
+#' }
 #' @references This data set was artificially created for the Rinteract package.
 #' @keywords datasets
-
+#'
 "toydata"
 
