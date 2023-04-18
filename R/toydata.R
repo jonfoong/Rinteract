c9f8d5aea441f323e29e47e96b2720e9cd903dbf
jonfoong <jonahfoong@gmail.com>
2023-04-18 16:27:29 +0200

    add toydata

---
+++ R/toydata.R
@@ -0,0 +1,15 @@
+#' Toy data with interactions
+#'
+#' Small, artificially generated toy data set with interactions variables and fixed effect clusters
+#'
+#' @docType data
+#'
+#' @usage data(toydata)
+#'
+#' @format An object of class \code{"data.frame"}
+
+#' @references This data set was artificially created for the Rinteract package.
+#' @keywords datasets
+
+"toydata"
+
