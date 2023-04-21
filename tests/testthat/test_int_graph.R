09a7fd708b3fd349586c434370f01dc99730477c
jonfoong <jonahfoong@gmail.com>
2023-04-21 13:15:07 +0200

    readme and test

--- tests/testthat/test_int_graph.R
+++ tests/testthat/test_int_graph.R
@@ -7,15 +7,12 @@ test_that("no facet triggers error", {
   expect_error(int_graph(out), "facet argument cannot be empty!")
 })
 
-
 test_that("int_graph returns ggplot object", {
   mod <- lm(Y~X1*X2*X3, toydata)
   out <- int_conditions(mod, toydata)
   plot <- int_graph(out, X1+X2~X3)
   expect_true("ggplot" %in% class(plot))
 })
-
-
 
 
 
